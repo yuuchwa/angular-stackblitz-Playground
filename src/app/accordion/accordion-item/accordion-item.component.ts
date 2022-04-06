8e04e7cf1dfa6f4ab5455dcab931e4d1ac37df92
yuuchwa <yuuchwa@gmail.com>
2022-04-06 13:52:46 +0200

    2 files modified

--- src/app/accordion/accordion-item/accordion-item.component.ts
+++ src/app/accordion/accordion-item/accordion-item.component.ts
@@ -1,3 +1,4 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnInit, Input } from '@angular/core';
 import { Movie } from '../../Interface/Movie';
 
@@ -20,5 +21,16 @@ export class AccordionItemComponent implements OnInit {
 
   toggle() {
     this.showBody = !this.showBody;
+  }
+
+  onDdrop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
   }
 }
